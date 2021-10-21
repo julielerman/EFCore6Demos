561d3d40b958867f0a1a538963cb0ff07b4b5253
Julie Lerman <julielerman@users.noreply.github.com>
2021-10-21 07:50:26 -0400

    more cleaning

--- SOContext.cs
+++ SOContext.cs
@@ -1,54 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EFCore6Demos
 {
-    public  class SOContext : DbContext
-  
-    {
-  
-
+  public class SOContext : DbContext
+  {
     public SOContext(DbContextOptions<SOContext> options)
             : base(options)
-        {
-        }
-
-        public  DbSet<UserReputation> UserReps { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(
-                    "Server=;Database=StackOverflow2010;Trusted_Connection=True;");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //via scaffolding from database
-            modelBuilder.Entity<UserReputation>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Reputation");
-
-                entity.Property(e => e.Displayname)
-                    .IsRequired()
-                    .HasMaxLength(40)
-                    .HasColumnName("displayname");
-
-                entity.Property(e => e.Id)
-                    .ValueGeneratedOnAdd()
-                    .HasColumnName("id");
-
-                entity.Property(e => e.Reputation).HasColumnName("reputation");
-            });
-
-        }
-
-       
+    {
     }
+
+    public DbSet<UserReputation> UserReps { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseSqlServer(
+            "Server=;Database=StackOverflow2010;Trusted_Connection=True;");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      //via scaffolding from database
+      modelBuilder.Entity<UserReputation>(entity =>
+      {
+        entity.HasNoKey();
+        entity.ToView("Reputation");
+        entity.Property(e => e.Displayname)
+                  .IsRequired()
+                  .HasMaxLength(40)
+                  .HasColumnName("displayname");
+        entity.Property(e => e.Id)
+                  .ValueGeneratedOnAdd()
+                  .HasColumnName("id");
+        entity.Property(e => e.Reputation).HasColumnName("reputation");
+      });
+
+    }
+
+
+  }
 }

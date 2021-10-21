0afe763ca3b506dd202d9fc790598f6bcaee9c8a
Julie Lerman <julielerman@users.noreply.github.com>
2021-10-21 07:49:28 -0400

    clean context

--- SOContext.cs
+++ SOContext.cs
@@ -6,44 +6,26 @@ using Microsoft.EntityFrameworkCore.Metadata;
 namespace EFCore6Demos
 {
     public  class SOContext : DbContext
-  
-    {
-  
-
-    public SOContext(DbContextOptions<SOContext> options)
+      {
+     public SOContext(DbContextOptions<SOContext> options)
             : base(options)
         {
         }
-
         public  DbSet<UserReputation> UserReps { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(
-                    "Server=;Database=StackOverflow2010;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //via scaffolding from database
             modelBuilder.Entity<UserReputation>(entity =>
             {
                 entity.HasNoKey();
-
                 entity.ToView("Reputation");
-
                 entity.Property(e => e.Displayname)
                     .IsRequired()
                     .HasMaxLength(40)
                     .HasColumnName("displayname");
-
                 entity.Property(e => e.Id)
                     .ValueGeneratedOnAdd()
                     .HasColumnName("id");
-
                 entity.Property(e => e.Reputation).HasColumnName("reputation");
             });
 

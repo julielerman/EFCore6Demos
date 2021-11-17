80a308286ed40683d44a4465b23053b16924da55
Julie Lerman <julielerman@users.noreply.github.com>
2021-11-17 09:26:11 -0500

    mod demo data

--- Program.cs
+++ Program.cs
@@ -16,25 +16,19 @@ namespace EFCore6Demos
       _context.Database.EnsureDeleted();
       _context.Database.EnsureCreated();
       InsertAddress();
-      InsertCyrillicAddress();
       ReadAddresswithValueconverters();
     }
 
     private static void InsertAddress()
     {
-      var add1 = new Address {City="北京", Street = "1 Main", BuildingColor = System.Drawing.Color.Blue, AddressTypeEnum = AddressTypeEnum.Home };
+      var add1 = new Address {City="Vilnius", Street = "Gediminas Avenue",
+         BuildingColor = System.Drawing.Color.Blue,
+          AddressTypeEnum = AddressTypeEnum.Home };
       _context.Set<Address>().Add(add1);
       _context.SaveChanges();
     }
 
-    private static void InsertCyrillicAddress()
-    {
-      var martaStreet = new Address { Street = "Улица 8 марта", AddressTypeEnum = AddressTypeEnum.Home };
-      _context.Set<Address>().Add(martaStreet);
-      _context.SaveChanges();
-    }
-
-    
+     
     private static void ReadAddresswithValueconverters()
     {
       var addresses = _context.Set<Address>().ToList();
